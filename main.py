1dc722a87998039335e70b09112d9544d60fd2c6
Alexander Vedernikov <alexander.vedernikov@viableone.com>
2023-02-27 16:49:39 +0100

    v2.1-alpha

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import random
 import itertools
 import time
-import numpy
 import multiprocessing
+import configparser
 
-
-def get_items_count_user_input():
-    itemCountInput = ""
-    print("How much items do you want to generate for solving?")
-    while itemCountInput == "":
-        itemCountInput = input(">> ")
-        try:
-            itemCount = int(itemCountInput)
-            return itemCount
-        except ValueError:
-            print("Only numbers allowed")
-            itemCountInput = ""
+""" Loading configuration file as 'config' """
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 
 def generate_items(itemCount, wMin, wMax, vMin, vMax):
+    """
+    Generating knapsack items as two lists of 'weights', 'values' based on inputs
+    :param itemCount: Count of items that will be generated
+    :param wMin: Minimal value of range of weights
+    :param wMax: Maximal value of range of weights
+    :param vMin: Minimal value of range of values
+    :param vMax:Maximal value of range of values
+    :return: Tuple of two lists as weights and values
+    """
     # item creation
     weights = []
     values = []
@@ -28,57 +28,20 @@ def generate_items(itemCount, wMin, wMax, vMin, vMax):
         print(i, weights[i], values[i])
     return weights, values
 
-
-def get_min_max_user_input():
-    inputString = ""
-    min = 0
-    max = 0
-    while inputString == "":
-        inputString = input(">> ")
-        try:
-            numbers = inputString.split(",")
-            if len(numbers) == 2:
-                num1 = int(numbers[0])
-                num2 = int(numbers[1])
-                if num1 > num2:
-                    min = num2
-                    max = num1
-                else:
-                    min = num1
-                    max = num2
-            else:
-                print("Exactly two numbers required!")
-                inputString = ""
-        except ValueError:
-            print("Only numbers allowed!")
-            inputString = ""
-    return min, max
-
-
 # brute force algorithm
-def knapsack_bruteforce(weights, values, itemCount, knapsackCapacity):
-    maxValue = 0
-    bestCombination = []
-    for i in range(1, itemCount + 1):
-        for combination in itertools.combinations(range(itemCount), i):
-            totalWeight = sum(weights[j] for j in combination)
-            if totalWeight <= knapsackCapacity:
-                totalValue = sum(values[j] for j in combination)
-                if totalValue > maxValue:
-                    maxValue = totalValue
-                    bestCombination = combination
-    if not bestCombination:
-        return None
-
-    return maxValue, list(bestCombination)
-
-
-# combinations algorithm
 def calc_combination(knapsackCapacity, weights, values, starIndex, endIndex, combinations, queue):
-    print("invoke calc")
+    """
+    algorithm for parallelized bruteforce that will compare combinations one by one and put it's chunnk into queue
+    :param knapsackCapacity: Capacity of knapsack
+    :param weights: List of weights
+    :param values: List of values
+    :param starIndex: index to start from
+    :param endIndex: index to end
+    :param combinations: list of combinations
+    :param queue: queue to put the process result
+    """
     maxValue = 0
     bestCombination = []
-    print(starIndex, endIndex)
     for i in range(starIndex, endIndex):
         combination = combinations[i]
         totalWeight = sum(weights[j] for j in combination)
@@ -92,6 +55,15 @@ def calc_combination(knapsackCapacity, weights, values, starIndex, endIndex, com
 
 
 def knapsack_bruteforce_processing(weights, values, itemCount, knapsackCapacity):
+    """
+    Parallelized bruteforce algorithm that will generate all combinations of all available items and compare them one by
+    one in different processes using 'calc_combination' function
+    :param weights: List of weights
+    :param values: List of values
+    :param itemCount: Count of items
+    :param knapsackCapacity: Available capacity of knapsack
+    :return: Best Value of combination and list of items numbers that are inside a combination
+    """
     cpuCount = multiprocessing.cpu_count()
     queue = multiprocessing.Queue()
     maxValue = 0
@@ -104,13 +76,13 @@ def knapsack_bruteforce_processing(weights, values, itemCount, knapsackCapacity)
     processes = []
     partSize = len(combinations) // cpuCount
     for i in range(cpuCount):
-        print("invoke process")
         start = i * partSize
         end = start + partSize
 
         process = multiprocessing.Process(target=calc_combination, args=(knapsackCapacity, weights, values, start, end, combinations, queue))
         process.start()
         processes.append(process)
+        print(len(processes))
 
     for process in processes:
         process.join()
@@ -124,41 +96,31 @@ def knapsack_bruteforce_processing(weights, values, itemCount, knapsackCapacity)
     return maxValue, list(bestCombination)
 
 
+""" Main of application """
 if __name__ == "__main__":
-    itemCount = get_items_count_user_input()
+    """ Getting values from config files """
+    itemCount = int(config['Test1']['itemCount'])
+    vMin = int(config['Test1']['vMin'])
+    vMax = int(config['Test1']['vMax'])
+    wMin = int(config['Test1']['wMin'])
+    wMax = int(config['Test1']['wMax'])
+    knapsackCapacity = int(config['Test1']['knapsackCapacity'])
 
-    print("What range of values you want to generate an items with?")
-    print("Use ',' between two numbers!")
-    vMin, vMax = get_min_max_user_input()
-
-    print("What range of weights you want to generate an items with?")
-    print("Use ',' between two numbers!")
-    wMin, wMax = get_min_max_user_input()
-
+    """ Returning generated values """
     weights, values = generate_items(itemCount, wMin, wMax, vMin, vMax)
 
-    knapsackCapacity = 15
 
-    # Non parallelized
-    timeStart = time.time()
-    theBest = knapsack_bruteforce(weights, values, itemCount, knapsackCapacity)
-    timeStop = time.time()
+    def run_bruteforce_parallelized():
+        """Executing 'knapsack_bruteforce_processing' with time measuring and printing a result with time"""
+        timeStart2 = time.time()
+        theBestParallel = knapsack_bruteforce_processing(weights, values, itemCount, knapsackCapacity)
+        timeStop2 = time.time()
 
-    timeResult = timeStop - timeStart
+        timeResultParallelized = timeStop2 - timeStart2
 
-
-    print("Non parallelized")
-    print(theBest)
-    print("TIME " + str(timeResult))
-
-    # parallelized
-    timeStart2 = time.time()
-    theBestParallel = knapsack_bruteforce_processing(weights, values, itemCount, knapsackCapacity)
-    timeStop2 = time.time()
-
-    timeResultParallelized = timeStop2 - timeStart2
+        print("parallelized")
+        print(theBestParallel)
+        print("TIME " + str(timeResultParallelized))
 
 
-    print("parallelized")
-    print(theBestParallel)
-    print("TIME " + str(timeResultParallelized))
+    run_bruteforce_parallelized()
