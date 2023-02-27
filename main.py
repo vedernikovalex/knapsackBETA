603057d68c08149b022caf461a200fa5749c13cd
Alexander Vedernikov <alexander.vedernikov@viableone.com>
2023-02-27 16:57:55 +0100

    v3.1

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ import time
 import multiprocessing
 import configparser
 
+""" Loading configuration file as 'config' """
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 
 def generate_items(itemCount, wMin, wMax, vMin, vMax):
+    """
+    Generating knapsack items as two lists of 'weights', 'values' based on inputs
+    :param itemCount: Count of items that will be generated
+    :param wMin: Minimal value of range of weights
+    :param wMax: Maximal value of range of weights
+    :param vMin: Minimal value of range of values
+    :param vMax:Maximal value of range of values
+    :return: Tuple of two lists as weights and values
+    """
     # item creation
     weights = []
     values = []
@@ -16,79 +26,19 @@ def generate_items(itemCount, wMin, wMax, vMin, vMax):
         weights.append(random.randint(wMin, wMax))
         values.append(random.randint(vMin, vMax))
         print(i, weights[i], values[i])
-    return weights, values
+    return weights,
 
 
 # brute force algorithm
-def knapsack_bruteforce(weights, values, itemCount, knapsackCapacity):
-    maxValue = 0
-    bestCombination = []
-    for i in range(1, itemCount + 1):
-        for combination in itertools.combinations(range(itemCount), i):
-            totalWeight = sum(weights[j] for j in combination)
-            if totalWeight <= knapsackCapacity:
-                totalValue = sum(values[j] for j in combination)
-                if totalValue > maxValue:
-                    maxValue = totalValue
-                    bestCombination = combination
-    if not bestCombination:
-        return None
-
-    return maxValue, list(bestCombination)
-
-
-# combinations algorithm
-def calc_combination(knapsackCapacity, weights, values, starIndex, endIndex, combinations, queue):
-    print("invoke calc")
-    maxValue = 0
-    bestCombination = []
-    print(starIndex, endIndex)
-    for i in range(starIndex, endIndex):
-        combination = combinations[i]
-        totalWeight = sum(weights[j] for j in combination)
-        if totalWeight <= knapsackCapacity:
-            totalValue = sum(values[j] for j in combination)
-            if totalValue > maxValue:
-                maxValue = totalValue
-                bestCombination = combination
-
-    queue.put((maxValue, bestCombination))
-
-
-def knapsack_bruteforce_processing(weights, values, itemCount, knapsackCapacity):
-    cpuCount = multiprocessing.cpu_count()
-    queue = multiprocessing.Queue()
-    maxValue = 0
-    bestCombination = []
-    combinations = []
-    for i in range(1, itemCount + 1):
-        itemCombinations = itertools.combinations(range(itemCount), i)
-        combinations.extend(itemCombinations)
-
-    processes = []
-    partSize = len(combinations) // cpuCount
-    for i in range(cpuCount):
-        print("invoke process")
-        start = i * partSize
-        end = start + partSize
-
-        process = multiprocessing.Process(target=calc_combination, args=(knapsackCapacity, weights, values, start, end, combinations, queue))
-        process.start()
-        processes.append(process)
-
-    for process in processes:
-        process.join()
-
-    for i in range(len(processes)):
-        result = queue.get()
-        if result[0] > maxValue:
-            maxValue = result[0]
-            bestCombination = result[1]
-
-    return maxValue, list(bestCombination)
-
-
 def greedy_algorithm(weights, values, capacity):
+    """
+    Greedy heuristic algorithm that will sort items by its coefficient of value:weight and choosing the best possible
+    combination that can fit in knapsack and continues to the next item untill its full
+    :param weights: List of weights
+    :param values: List of values
+    :param capacity: Capacity of knapsack
+    :return: Best Value of combination and list of items numbers that are inside a combination
+    """
     valueWeightCoeficient = []
     for v, w in zip(values, weights):
         valueWeightCoeficient.append((v / w, v, w))
@@ -110,7 +60,39 @@ def greedy_algorithm(weights, values, capacity):
                 return value2, secondCombination
 
 
+def dynamic_knapsack(weights, values, itemCount, capacity):
+    """
+    Optimized Algorithm for solving knapsack problem by checking only items that can be fitted inside knapsack
+    it calculates total value and weight and compares each combination with current best combination
+    :param weights: List of weights
+    :param values: List of values
+    :param itemCount: Count of all items
+    :param capacity: Knapsack capacity
+    :return: Best Value of combination and list of items numbers that are inside a combination
+    """
+    if values[0] <= capacity:
+        bestCombination = [(0, 0, set()), (values[0], weights[0], {1})]
+    else:
+        bestCombination = [(0, 0, set())]
+    for i in range(0, itemCount):
+        temp = bestCombination
+        secondCombination = []
+        for k, w, T in temp:
+            if w + weights[i] <= capacity:
+                secondCombination.append((k + values[i], w + weights[i], T.union({i + 1})))
+        temp.extend(secondCombination)
+        temp = sorted(temp, key=lambda x: (x[0], -x[1]))
+        bestCombination = [temp[0]]
+        for j in range(1, len(temp)):
+            if temp[j][0] != temp[j-1][0]:
+                bestCombination.append(temp[j])
+    m, w, T = max(bestCombination, key=lambda x: x[0])
+    return m, list(T)
+
+
+""" Main of application """
 if __name__ == "__main__":
+    """ Getting values from config files """
     itemCount = int(config['Test1']['itemCount'])
     vMin = int(config['Test1']['vMin'])
     vMax = int(config['Test1']['vMax'])
@@ -118,36 +100,34 @@ if __name__ == "__main__":
     wMax = int(config['Test1']['wMax'])
     knapsackCapacity = int(config['Test1']['knapsackCapacity'])
 
+    """ Returning generated values """
     weights, values = generate_items(itemCount, wMin, wMax, vMin, vMax)
 
-    # Non parallelized
-    timeStart = time.time()
-    theBest = knapsack_bruteforce(weights, values, itemCount, knapsackCapacity)
-    timeStop = time.time()
+    def run_greedy():
+        """Executing 'greedy_algorithm' with time measuring and printing a result with time"""
+        timeStartGreedy = time.time()
+        theBestGreedy = greedy_algorithm(weights, values, knapsackCapacity)
+        timeStopGreedy = time.time()
 
-    timeResult = timeStop - timeStart
+        timeResultGreedy = timeStopGreedy - timeStartGreedy
 
-    print("Non parallelized")
-    print(theBest)
-    print("TIME " + str(timeResult))
+        print("Greedy heuristic")
+        print(theBestGreedy)
+        print("TIME " + str(timeResultGreedy))
 
-    # parallelized
-    #timeStart2 = time.time()
-    #theBestParallel = knapsack_bruteforce_processing(weights, values, itemCount, knapsackCapacity)
-    #timeStop2 = time.time()
+    # dynamic
+    def run_dynamic():
+        """Executing 'dynamic_knapsack' with time measuring and printing a result with time"""
+        timeStartDynamic = time.time()
+        theBestDynamic = dynamic_knapsack(weights, values, itemCount, knapsackCapacity)
+        timeStopDynamic = time.time()
 
-    #timeResultParallelized = timeStop2 - timeStart2
+        timeResultDynamic = timeStopDynamic - timeStartDynamic
 
-    #print("parallelized")
-    #print(theBestParallel)
-    #print("TIME " + str(timeResultParallelized))
+        print("Dynamic")
+        print(theBestDynamic)
+        print("TIME " + str(timeResultDynamic))
 
-    timeStartGreedy = time.time()
-    theBestGreedy = greedy_algorithm(weights, values, knapsackCapacity)
-    timeStopGreedy = time.time()
 
-    timeResultGreedy = timeStopGreedy - timeStartGreedy
-
-    print("Greedy heuristic")
-    print(theBestGreedy)
-    print("TIME " + str(timeResultGreedy))
+    run_greedy()
+    run_dynamic()
