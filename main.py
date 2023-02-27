68f82d69e03a67e8469d6f3f51c7fb3f684ce268
Alexander Vedernikov <alexander.vedernikov@viableone.com>
2023-02-27 16:37:47 +0100

    v1.2

--- main.py
+++ main.py
@@ -1,88 +1,83 @@
 import random
 import itertools
 import time
+import configparser
 
-weights = []
-values = []
-
-itemCountInput = ""
-itemCount = 0
-print("How much items do you want to generate for solving?")
-while itemCountInput == "":
-    itemCountInput = input(">> ")
-    try:
-        itemCount = int(itemCountInput)
-    except ValueError:
-        print("Only numbers allowed")
-        itemCountInput = ""
+""" Loading configuration file as 'config' """
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 
-def get_min_max_user_input():
-    inputString = ""
-    min = 0
-    max = 0
-    while inputString == "":
-        inputString = input(">> ")
-        try:
-            numbers = inputString.split(",")
-            if len(numbers) <= 2:
-                num1 = int(numbers[0])
-                num2 = int(numbers[1])
-                if num1 > num2:
-                    min = num2
-                    max = num1
-                else:
-                    min = num1
-                    max = num2
-            else:
-                print("More than two numbers inserted")
-                inputString = ""
-        except ValueError:
-            print("Only numbers allowed")
-            inputString = ""
-    return min, max
-
-
-print("What range of values you want to generate an items with?")
-print("Use ',' between two numbers!")
-vMin, vMax = get_min_max_user_input()
-
-print("What range of weights you want to generate an items with?")
-print("Use ',' between two numbers!")
-wMin, wMax = get_min_max_user_input()
-
-# item creation
-for i in range(itemCount):
-    weights.append(random.randint(wMin, wMax))
-    values.append(random.randint(vMin, vMax))
-    print(i, weights[i], values[i])
-
-knapsackCapacity = 15
+def generate_items(itemCount, wMin, wMax, vMin, vMax):
+    """
+    Generating knapsack items as two lists of 'weights', 'values' based on inputs
+    :param itemCount: Count of items that will be generated
+    :param wMin: Minimal value of range of weights
+    :param wMax: Maximal value of range of weights
+    :param vMin: Minimal value of range of values
+    :param vMax:Maximal value of range of values
+    :return: Tuple of two lists as weights and values
+    """
+    # item creation
+    weights = []
+    values = []
+    for i in range(itemCount):
+        weights.append(random.randint(wMin, wMax))
+        values.append(random.randint(vMin, vMax))
+        print(i, weights[i], values[i])
+    return weights, values
 
 
 # brute force algorithm
-def knapsack_bruteforce(values, weights, capacity):
+def knapsack_bruteforce(weights, values, itemCount, knapsackCapacity):
+    """
+    Simple bruteforce algorithm that will generate all combinations of all available items and compare them one by one
+    :param weights: List of weights
+    :param values: List of values
+    :param itemCount: Count of items
+    :param knapsackCapacity: Available capacity of knapsack
+    :return: Best Value of combination and list of items that are inside a combination
+    """
     maxValue = 0
-    best_combination = []
+    bestCombination = []
     for i in range(1, itemCount + 1):
         for combination in itertools.combinations(range(itemCount), i):
             totalWeight = sum(weights[j] for j in combination)
-            if totalWeight <= capacity:
+            if totalWeight <= knapsackCapacity:
                 totalValue = sum(values[j] for j in combination)
                 if totalValue > maxValue:
                     maxValue = totalValue
-                    best_combination = combination
-    if not best_combination:
+                    bestCombination = combination
+    if not bestCombination:
         return None
 
-    return maxValue, list(best_combination)
+    return maxValue, list(bestCombination)
 
 
-timeStart = time.time()
-theBest = knapsack_bruteforce(values, weights, knapsackCapacity)
-timeStop = time.time()
+""" Main of application """
+if __name__ == "__main__":
+    """ Getting values from config files """
+    itemCount = int(config['Test1']['itemCount'])
+    vMin = int(config['Test1']['vMin'])
+    vMax = int(config['Test1']['vMax'])
+    wMin = int(config['Test1']['wMin'])
+    wMax = int(config['Test1']['wMax'])
+    knapsackCapacity = int(config['Test1']['knapsackCapacity'])
 
-timeResult = timeStop - timeStart
+    """ Returning generated values """
+    weights, values = generate_items(itemCount, wMin, wMax, vMin, vMax)
 
-print(theBest)
-print("TIME " + str(timeResult))
+
+    def run_bruteforce():
+        """Executing 'knapsack_bruteforce' with time measuring and printing a result with time"""
+        timeStart = time.time()
+        theBest = knapsack_bruteforce(weights, values, itemCount, knapsackCapacity)
+        timeStop = time.time()
+
+        timeResult = timeStop - timeStart
+
+        print("Non parallelized")
+        print(theBest)
+        print("TIME " + str(timeResult))
+
+    run_bruteforce()
